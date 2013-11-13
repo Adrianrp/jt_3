bb42db93659969e34518ed3334b8d68934cc60c2
Adrianrp <info@medula.dk>
2013-11-13 23:28:35 +0100

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,8 +11,13 @@ describe "Static pages" do
 
     it "should have the right title" do
 	  visit '/static_pages/home'
-	  expect(page).to have_title("Jespers Torvekøkken | Home")
+	  expect(page).to have_title("Jespers Torvekøkken")
 	end
+
+	it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
   end
 
   describe "Menus page" do
