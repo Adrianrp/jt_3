314e274ec75b111ecdf21af48df4a04fbb4a81e8
Adrianrp <info@medula.dk>
2013-11-25 20:55:14 +0100

    finish product-display

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
+  	# finds the number of records specified by limit ordered by primary key 
+  	# in descending order
+  	@products = Product.last(3)
   end
 
   def menus
   	@products = Product.where(category_id: 1)
+  	# @season = Product.where(category_id: 3)
   end
 
   def buffets
+  	@products = Product.where(category_id: 2)
   end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+
+	# Receives a Image hash and returns the URL of that image
+	def give_me_image(image)
+		image.each do |image|
+			return image.image
+		end
+	end
 end
