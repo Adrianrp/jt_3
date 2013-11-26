ed1409fddee2db6a6aecab92ba85688e6ed5a2b2
Adrianrp <info@medula.dk>
2013-11-26 21:10:18 +0100

    finished validation products

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,9 @@
 class Image < ActiveRecord::Base
 	belongs_to :product
+
+	validates :image, :product_id, presence: true
+	validates :image, allow_blank: true, format: {
+		with:  %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image'
+	}
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
 	has_many :images
 	belongs_to :category
+
+	validates :image_id, :title, :description, :price, :category_id,  presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
 end
